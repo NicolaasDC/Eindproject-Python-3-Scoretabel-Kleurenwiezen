7a31c99a474dbf41404631a65f888c56664f97cf
NicolaasDC <167005737+NicolaasDC@users.noreply.github.com>
2024-06-10 13:20:05 +0200

    Input van de variabelen bij nieuw spel verder op punt gestelt en uitgetest of de correcte scores terug gegeven worden. Bij foute input komt een foutmelding

--- Source/klassen.py
+++ Source/klassen.py
@@ -5,7 +5,7 @@ class Kleurenwiezen():
         self.speler2 = speler2
         self.speler3 = speler3
         self.speler4 = speler4
-        self.scores = {speler1: 0, self.speler2: 0, self.speler3: 0, self.speler4: 0}
+        self.scores = {speler1: 0, speler2: 0, speler3: 0, speler4: 0}
 
     def __repr__(self):
         return f"{self.__class__.__name__}({self.speler1!r}, {self.speler2!r}, {self.speler3!r}, {self.speler4!r})"
@@ -34,76 +34,76 @@ class Kleurenwiezen():
             "Abondance 11": {"vrager": [60], "niet_vrager": [-20]},
             "Blote miserie": {"vrager": [75], "niet_vrager": [-75]},
             "Abondance 12": {"vrager": [120], "niet_vrager": [-40]},
-            "Soloslim": {"vrager": [240], "niet_vrager": [-80]}
+            "Solo slim": {"vrager": [240], "niet_vrager": [-80]}
         }
 
         puntenverdeling_vrager_mislukt ={
-            "Samen 8": {"vrager": [-10, -10, -13, -16, -19, -22], "niet_vrager": [10, 10, 13, 16, 19, 22]},
-            "Solo 5": {"vrager": [-12, -12, -15, -18, -21, -24],"niet_vrager": [4, 5, 6, 7, 8]},
-            "Samen 9": {"vrager": [-13, -13, -16, -19, -22, -25], "niet_vrager": [13, 16, 19, 22, 25]},
-            "Solo 6": {"vrager": [-15, -15, -18, -21, -24, -27], "niet_vrager": [5, 6, 7, 8, 9]},
-            "Samen 10": {"vrager": [-16, -16, -19, -22, -25], "niet_vrager": [16, 19, 22, 25]},
-            "Solo 7": {"vrager": [-18, -18, -21, -24, -27, -30], "niet_vrager": [6, 7, 8, 9, 10]},
-            "Samen 11": {"vrager": [-19, -19, -22, -25, -28, -31], "niet_vrager": [19, 22, 25, 28, 31]},
+            "Samen 8": {"vrager": [0, -10, -13, -16, -19, -22], "niet_vrager": [0, 10, 13, 16, 19, 22]},
+            "Solo 5": {"vrager": [0, -12, -15, -18, -21, -24],"niet_vrager": [0, 4, 5, 6, 7, 8]},
+            "Samen 9": {"vrager": [0, -13, -16, -19, -22, -25], "niet_vrager": [0, 13, 16, 19, 22, 25]},
+            "Solo 6": {"vrager": [0, -15, -18, -21, -24, -27], "niet_vrager": [0, 5, 6, 7, 8, 9]},
+            "Samen 10": {"vrager": [0, -16, -19, -22, -25], "niet_vrager": [0, 16, 19, 22, 25]},
+            "Solo 7": {"vrager": [0, -18, -21, -24, -27, -30], "niet_vrager": [0, 6, 7, 8, 9, 10]},
+            "Samen 11": {"vrager": [0, -19, -22, -25, -28, -31], "niet_vrager": [0, 19, 22, 25, 28, 31]},
             "Kleine miserie": {"vrager": [-18], "niet_vrager": [6]},
-            "Samen 12": {"vrager": [-22, -22, -25, -28, -31, -34], "niet_vrager": [19, 22, 25, 28, 31, 34]},
-            "Solo 8": {"vrager": [-21, -21, -24, -27, -30, -33], "niet_vrager": [7, 8, 9, 10, 11]},
-            "Samen 13": {"vrager": [-30, -30, -33, -36, -39, -42, -45 ], "niet_vrager": [30, 30, 33, 36, 39, 42, 45]},
+            "Samen 12": {"vrager": [0, -22, -25, -28, -31, -34], "niet_vrager": [0, 22, 25, 28, 31, 34]},
+            "Solo 8": {"vrager": [0, -21, -24, -27, -30, -33], "niet_vrager": [7, 8, 9, 10, 11]},
+            "Samen 13": {"vrager": [0, -30, -33, -36, -39, -42, -45 ], "niet_vrager": [0, 30, 33, 36, 39, 42, 45]},
             "Abondance 9": {"vrager": [33], "niet_vrager": [-11]},
-            "Troel voor 8": {"vrager": [-16], "niet_vrager": [16]},
-            "Troel voor 9": {"vrager": [-16], "niet_vrager": [16]},
+            "Troel voor 8": {"vrager": [-16, -16, -16, -16, 16, -16, -16, -16], "niet_vrager": [16, 16, 16, 16, 16, 16, 16, 16]},
+            "Troel voor 9": {"vrager": [-16, -16, -16, -16, 16, -16, -16, -16], "niet_vrager": [16, 16, 16, 16, 16, 16, 16, 16]},
             "Grote miserie": {"vrager": [-36], "niet_vrager": [12]},
             "Abondance 10": {"vrager": [-42], "niet_vrager": [42]},
             "Abondance 11": {"vrager": [-60], "niet_vrager": [20]},
             "Blote miserie": {"vrager": [-75], "niet_vrager": [75]},
             "Abondance 12": {"vrager": [-120], "niet_vrager": [40]},
-            "Soloslim": {"vrager": [-240], "niet_vrager": [80]}
+            "Solo slim": {"vrager": [-240], "niet_vrager": [80]}
         }
 
         if speler == self.speler1:
             if vrager == True:
                 if gelukt == True:
-                    self.scores['speler1'] += puntenverdeling_vrager_gelukt[spel]['vrager'][overslagen]
+                    self.scores[speler] += puntenverdeling_vrager_gelukt[spel]['vrager'][overslagen]
                 if gelukt == False:
-                    self.scores['speler1'] += puntenverdeling_vrager_mislukt[spel]['vrager'][overslagen]
+                    self.scores[speler] += puntenverdeling_vrager_mislukt[spel]['vrager'][overslagen]
             if vrager == False:
                 if gelukt == True:
-                    self.scores['speler1'] += puntenverdeling_vrager_gelukt[spel]['niet_vrager'][overslagen]
+                    self.scores[speler] += puntenverdeling_vrager_gelukt[spel]['niet_vrager'][overslagen]
                 if gelukt == False:
-                    self.scores['speler1'] += puntenverdeling_vrager_mislukt[spel]['niet_vrager'][overslagen]
+                    self.scores[speler] += puntenverdeling_vrager_mislukt[spel]['niet_vrager'][overslagen]
 
         if speler == self.speler2:
             if vrager == True:
                 if gelukt == True:
-                    self.scores['speler2'] += puntenverdeling_vrager_gelukt[spel]['vrager'][overslagen]
+                    self.scores[speler] += puntenverdeling_vrager_gelukt[spel]['vrager'][overslagen]
                 if gelukt == False:
-                    self.scores['speler2'] += puntenverdeling_vrager_mislukt[spel]['vrager'][overslagen]
+                    self.scores[speler] += puntenverdeling_vrager_mislukt[spel]['vrager'][overslagen]
             if vrager == False:
                 if gelukt == True:
-                    self.scores['speler2'] += puntenverdeling_vrager_gelukt[spel]['niet_vrager'][overslagen]
+                    self.scores[speler] += puntenverdeling_vrager_gelukt[spel]['niet_vrager'][overslagen]
                 if gelukt == False:
-                    self.scores['speler2'] += puntenverdeling_vrager_mislukt[spel]['niet_vrager'][overslagen]
+                    self.scores[speler] += puntenverdeling_vrager_mislukt[spel]['niet_vrager'][overslagen]
 
         if speler == self.speler3:
             if vrager == True:
                 if gelukt == True:
-                    self.scores['speler3'] += puntenverdeling_vrager_gelukt[spel]['vrager'][overslagen]
+                    self.scores[speler] += puntenverdeling_vrager_gelukt[spel]['vrager'][overslagen]
                 if gelukt == False:
-                    self.scores['speler3'] += puntenverdeling_vrager_mislukt[spel]['vrager'][overslagen]
+                    self.scores[speler] += puntenverdeling_vrager_mislukt[spel]['vrager'][overslagen]
             if vrager == False:
                 if gelukt == True:
-                    self.scores['speler3'] += puntenverdeling_vrager_gelukt[spel]['niet_vrager'][overslagen]
+                    self.scores[speler] += puntenverdeling_vrager_gelukt[spel]['niet_vrager'][overslagen]
                 if gelukt == False:
-                    self.scores['speler3'] += puntenverdeling_vrager_mislukt[spel]['niet_vrager'][overslagen]
+                    self.scores[speler] += puntenverdeling_vrager_mislukt[spel]['niet_vrager'][overslagen]
 
         if speler == self.speler4:
             if vrager == True:
                 if gelukt == True:
-                    self.scores['speler4'] += puntenverdeling_vrager_gelukt[spel]['vrager'][overslagen]
+                    self.scores[speler] += puntenverdeling_vrager_gelukt[spel]['vrager'][overslagen]
                 if gelukt == False:
-                    self.scores['speler4'] += puntenverdeling_vrager_mislukt[spel]['vrager'][overslagen]
+                    self.scores[speler] += puntenverdeling_vrager_mislukt[spel]['vrager'][overslagen]
             if vrager == False:
                 if gelukt == True:
-                    self.scores['speler4'] += puntenverdeling_vrager_gelukt[spel]['niet_vrager'][overslagen]
+                    self.scores[speler] += puntenverdeling_vrager_gelukt[spel]['niet_vrager'][overslagen]
                 if gelukt == False:
-                    self.scores['speler4'] += puntenverdeling_vrager_mislukt[spel]['niet_vrager'][overslagen]
+                    self.scores[speler] += puntenverdeling_vrager_mislukt[spel]['niet_vrager'][overslagen]
--- User.py
+++ User.py
@@ -4,12 +4,13 @@ from tkinter import simpledialog, messagebox
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 
+rondes = []
+
 # Initialiseer de globale variabelen
 Speler1 = ""
 Speler2 = ""
 Speler3 = ""
 Speler4 = ""
-
 
 def create_window(player_num, submit_command):
     root = ttk.Window(themename="superhero")
@@ -67,229 +68,255 @@ Speler4 = 'Jonas'
 kleurenwiezen = Kleurenwiezen(Speler1, Speler2, Speler3, Speler4)
 
 
-# kleurenwiezen.update_score('speler1', 'Grote miserie', vrager=True, gelukt=True)
-# kleurenwiezen.update_score('speler2', 'Grote miserie', vrager=False, gelukt=True)
-# kleurenwiezen.update_score('speler3', 'Grote miserie', vrager=False, gelukt=True)
-# kleurenwiezen.update_score('speler4', 'Grote miserie', vrager=False, gelukt=True)
-
 
 # print(kleurenwiezen.score())
 
 
 def nieuw_spel():
-    def selecteer_spel():
-        selected_value = combo.get()
-        global spel
-        spel = selected_value
-        root.destroy()  # Close the window
-
-    # Create the main application window
-    root = tk.Tk()
-    root.title("Welk spel")
-    root.geometry("300x200")
-
-    # Create a label
-    label = tk.Label(root, text="Selecteer een spel:")
-    label.pack(pady=10)
-
-    # Create a combobox (drop-down menu)
-    options_spel = ["Samen 8", "Solo 5", "Samen 9", "Solo 6", "Samen 10", "Solo 7", "Samen 11", 'Kleine miserie',
-                    "Samen 12", "Solo 8", "Samen 13", "Abondance 9", "Troel voor 8", "Troel voor 9", "Grote miserie",
-                    "Abondance 10", "Abondance 11", "Abondance 12", "Solo slim"]
-    combo = ttk.Combobox(root, values=options_spel)
-    combo.pack(pady=10)
-
-    # Create a button to select the option and close the window
-    button = tk.Button(root, text="Select", command=selecteer_spel)
-    button.pack(pady=10)
-
-    # Run the application
-    root.mainloop()
-
-    twee_spelers = ["Samen 8", "Samen 9", "Samen 10", "Samen 11", "Samen 12", "Samen 13", "Troel voor 8",
-                    "Troel voor 9"]
-    options = [Speler1, Speler2, Speler3, Speler4]
-    # After the window is closed, you can use the selected_option variable
-    if spel in twee_spelers:
-        def selecteer_speler1():
+    try:
+        def selecteer_spel():
             selected_value = combo.get()
-            global speler1
-            speler1 = selected_value
-            options.remove(speler1)
+            global spel
+            spel = selected_value
             root.destroy()  # Close the window
 
         # Create the main application window
         root = tk.Tk()
-        root.title("Welk speler")
+        root.title("Welk spel")
         root.geometry("300x200")
 
         # Create a label
-        label = tk.Label(root, text="Selecteer speler 1:")
+        label = tk.Label(root, text="Selecteer een spel:")
         label.pack(pady=10)
 
         # Create a combobox (drop-down menu)
-        combo = ttk.Combobox(root, values=options)
+        options_spel = ["Samen 8", "Solo 5", "Samen 9", "Solo 6", "Samen 10", "Solo 7", "Samen 11", 'Kleine miserie',
+                        "Samen 12", "Solo 8", "Samen 13", "Abondance 9", "Troel voor 8", "Troel voor 9", "Grote miserie",
+                        "Abondance 10", "Abondance 11", "Abondance 12", "Solo slim"]
+        combo = ttk.Combobox(root, values=options_spel)
         combo.pack(pady=10)
 
         # Create a button to select the option and close the window
-        button = tk.Button(root, text="Select", command=selecteer_speler1)
+        button = tk.Button(root, text="Select", command=selecteer_spel)
         button.pack(pady=10)
 
         # Run the application
         root.mainloop()
 
-        def selecteer_speler2():
-            selected_value = combo.get()
-            global speler2
-            speler2 = selected_value
-            root.destroy()  # Close the window
-
-        # Create the main application window
-        root = tk.Tk()
-        root.title("Welk speler")
-        root.geometry("300x200")
-
-        # Create a label
-        label = tk.Label(root, text="Selecteer speler 2:")
-        label.pack(pady=10)
-
-        # Create a combobox (drop-down menu)
-        combo = ttk.Combobox(root, values=options)
-        combo.pack(pady=10)
-
-        # Create a button to select the option and close the window
-        button = tk.Button(root, text="Select", command=selecteer_speler2)
-        button.pack(pady=10)
-
-        # Run the application
-        root.mainloop()
-
-    else:
-        def selecteer_speler1():
-            selected_value = combo.get()
-            global speler1
-            speler1 = selected_value
-            root.destroy()  # Close the window
-
-        # Create the main application window
-        root = tk.Tk()
-        root.title("Welk speler")
-        root.geometry("300x200")
-
-        # Create a label
-        label = tk.Label(root, text="Selecteer speler 1:")
-        label.pack(pady=10)
-
-        # Create a combobox (drop-down menu)
+        twee_spelers = ["Samen 8", "Samen 9", "Samen 10", "Samen 11", "Samen 12", "Samen 13", "Troel voor 8",
+                        "Troel voor 9"]
         options = [Speler1, Speler2, Speler3, Speler4]
-        combo = ttk.Combobox(root, values=options)
+        # After the window is closed, you can use the selected_option variable
+        if spel in twee_spelers:
+            def selecteer_vrager1():
+                selected_value = combo.get()
+                global vrager1
+                vrager1 = selected_value
+                options.remove(vrager1)
+                root.destroy()  # Close the window
+
+            # Create the main application window
+            root = tk.Tk()
+            root.title("Welk speler")
+            root.geometry("300x200")
+
+            # Create a label
+            label = tk.Label(root, text="Selecteer vrager 1:")
+            label.pack(pady=10)
+
+            # Create a combobox (drop-down menu)
+            combo = ttk.Combobox(root, values=options)
+            combo.pack(pady=10)
+
+            # Create a button to select the option and close the window
+            button = tk.Button(root, text="Select", command=selecteer_vrager1)
+            button.pack(pady=10)
+
+            # Run the application
+            root.mainloop()
+
+            def selecteer_vrager2():
+                selected_value = combo.get()
+                global vrager2
+                vrager2 = selected_value
+                root.destroy()  # Close the window
+
+            # Create the main application window
+            root = tk.Tk()
+            root.title("Welk speler")
+            root.geometry("300x200")
+
+            # Create a label
+            label = tk.Label(root, text="Selecteer vrager 2:")
+            label.pack(pady=10)
+
+            # Create a combobox (drop-down menu)
+            combo = ttk.Combobox(root, values=options)
+            combo.pack(pady=10)
+
+            # Create a button to select the option and close the window
+            button = tk.Button(root, text="Select", command=selecteer_vrager2)
+            button.pack(pady=10)
+
+            # Run the application
+            root.mainloop()
+
+        else:
+            def selecteer_vrager1():
+                selected_value = combo.get()
+                global vrager1
+                vrager1 = selected_value
+                root.destroy()  # Close the window
+
+            # Create the main application window
+            root = tk.Tk()
+            root.title("Welk speler")
+            root.geometry("300x200")
+
+            # Create a label
+            label = tk.Label(root, text="Selecteer vrager:")
+            label.pack(pady=10)
+
+            # Create a combobox (drop-down menu)
+            options = [Speler1, Speler2, Speler3, Speler4]
+            combo = ttk.Combobox(root, values=options)
+            combo.pack(pady=10)
+
+            # Create a button to select the option and close the window
+            button = tk.Button(root, text="Select", command=selecteer_vrager1)
+            button.pack(pady=10)
+
+            # Run the application
+            root.mainloop()
+
+        def selecteer_geslaagd():
+            selected_value = combo.get()
+            global geslaagd
+            geslaagd = selected_value
+            if geslaagd == "Ja":
+                geslaagd = True
+            else:
+                geslaagd = False
+            root.destroy()  # Close the window
+
+        # Create the main application window
+        root = tk.Tk()
+        root.title("Spel geslaagd")
+        root.geometry("300x200")
+
+        # Create a label
+        label = tk.Label(root, text="Spel geslaagd?")
+        label.pack(pady=10)
+
+        # Create a combobox (drop-down menu)
+        options_geslaagd = ["Ja", "Nee"]
+        combo = ttk.Combobox(root, values=options_geslaagd)
         combo.pack(pady=10)
 
         # Create a button to select the option and close the window
-        button = tk.Button(root, text="Select", command=selecteer_speler1)
+        button = tk.Button(root, text="Select", command=selecteer_geslaagd)
         button.pack(pady=10)
 
         # Run the application
         root.mainloop()
 
-    def selecteer_geslaagd():
-        selected_value = combo.get()
-        global geslaagd
-        geslaagd = selected_value
-        if geslaagd == "Ja":
-            geslaagd = True
-        else:
-            geslaagd = False
-        root.destroy()  # Close the window
 
-    # Create the main application window
-    root = tk.Tk()
-    root.title("Spel geslaagd")
-    root.geometry("300x200")
+        spellen_overslagen = ["Samen 8", "Solo 5", "Samen 9", "Solo 6", "Samen 10", "Solo 7", "Samen 11", "Samen 12",
+                              "Troel voor 8", "Troel voor 9"]
 
-    # Create a label
-    label = tk.Label(root, text="Spel geslaagd?")
-    label.pack(pady=10)
-
-    # Create a combobox (drop-down menu)
-    options_geslaagd = ["Ja", "Nee"]
-    combo = ttk.Combobox(root, values=options_geslaagd)
-    combo.pack(pady=10)
-
-    # Create a button to select the option and close the window
-    button = tk.Button(root, text="Select", command=selecteer_geslaagd)
-    button.pack(pady=10)
-
-    # Run the application
-    root.mainloop()
-
-    spellen_overslagen = ["Samen 8", "Solo 5", "Samen 9", "Solo 6", "Samen 10", "Solo 7", "Samen 11", "Samen 12",
-                          "Troel voor 8", "Troel voor 9"]
-
-    if geslaagd == True and spel in spellen_overslagen:
-        def on_submit():
-            try:
-                # Get the input value and convert it to an integer
-                input_value = int(entry.get())
+        if geslaagd == True and spel in spellen_overslagen:
+            def selecteer_overslagen():
+                selected_value = combo.get()
                 global overslagen
-                overslagen = input_value
+                overslagen = selected_value
                 root.destroy()  # Close the window
-            except ValueError:
-                # Show an error message if the input is not a valid integer
-                messagebox.showerror("Invalid Input", "Please enter a valid integer.")
 
-        # Create the main application window
-        root = tk.Tk()
-        root.title("Vul de overslagen in")
-        root.geometry("300x150")
+            # Create the main application window
+            root = tk.Tk()
+            root.title("Hoeveel overslagen")
+            root.geometry("300x200")
 
-        # Create a label
-        label = tk.Label(root, text="Overslagen:")
-        label.pack(pady=10)
+            # Create a label
+            label = tk.Label(root, text="Hoeveel overslagen?")
+            label.pack(pady=10)
 
-        # Create an entry widget to input the integer
-        entry = tk.Entry(root)
-        entry.pack(pady=10)
+            # Create a combobox (drop-down menu)
+            options_overslagen = [0, 1, 2, 3, 4, 5]
+            combo = ttk.Combobox(root, values=options_overslagen)
+            combo.pack(pady=10)
 
-        # Create a button to submit the input and close the window
-        button = tk.Button(root, text="Submit", command=on_submit)
-        button.pack(pady=10)
+            # Create a button to select the option and close the window
+            button = tk.Button(root, text="Select", command=selecteer_overslagen)
+            button.pack(pady=10)
 
-        # Run the application
-        root.mainloop()
+            # Run the application
+            root.mainloop()
 
-    if geslaagd == False and spel in spellen_overslagen:
-        def on_submit():
-            try:
-                # Get the input value and convert it to an integer
-                input_value = int(entry.get())
+
+        if geslaagd == False and spel in spellen_overslagen:
+            def selecteer_overslagen():
+                selected_value = combo.get()
                 global overslagen
-                overslagen = input_value
+                overslagen = selected_value
                 root.destroy()  # Close the window
-            except ValueError:
-                # Show an error message if the input is not a valid integer
-                messagebox.showerror("Invalid Input", "Please enter a valid integer.")
 
-        # Create the main application window
-        root = tk.Tk()
-        root.title("Hoeveel slagen er in?")
-        root.geometry("300x150")
+            # Create the main application window
+            root = tk.Tk()
+            root.title("Hoeveel slagen erin?")
+            root.geometry("300x200")
 
-        # Create a label
-        label = tk.Label(root, text="Aantal slagen er in:")
-        label.pack(pady=10)
+            # Create a label
+            label = tk.Label(root, text="Hoeveel slagen erin?")
+            label.pack(pady=10)
 
-        # Create an entry widget to input the integer
-        entry = tk.Entry(root)
-        entry.pack(pady=10)
+            # Create a combobox (drop-down menu)
+            options_overslagen = [1, 2, 3, 4, 5, 6, 7, 8]
+            combo = ttk.Combobox(root, values=options_overslagen)
+            combo.pack(pady=10)
 
-        # Create a button to submit the input and close the window
-        button = tk.Button(root, text="Submit", command=on_submit)
-        button.pack(pady=10)
+            # Create a button to select the option and close the window
+            button = tk.Button(root, text="Select", command=selecteer_overslagen)
+            button.pack(pady=10)
 
-        # Run the application
-        root.mainloop()
+            # Run the application
+            root.mainloop()
+
+
+
+        spelers = [Speler1, Speler2, Speler3, Speler4]
+        twee_spelers = ["Samen 8", "Samen 9", "Samen 10", "Samen 11", "Samen 12", "Samen 13", "Troel voor 8",
+                        "Troel voor 9"]
+        if spel in twee_spelers:
+            kleurenwiezen.update_score(vrager1, spel, True, geslaagd, int(overslagen))
+            spelers.remove(vrager1)
+            kleurenwiezen.update_score(vrager2, spel, True, geslaagd, int(overslagen))
+            spelers.remove(vrager2)
+            kleurenwiezen.update_score(spelers[0], spel, False, geslaagd, int(overslagen))
+            kleurenwiezen.update_score(spelers[1], spel, False, geslaagd, int(overslagen))
+
+        if spel not in twee_spelers:
+            if spel in ["Solo 5", "Solo 6", "Solo 7", "Solo 8"]:
+                kleurenwiezen.update_score(vrager1, spel, True, geslaagd, int(overslagen))
+                spelers.remove(vrager1)
+                kleurenwiezen.update_score(spelers[0], spel, False, geslaagd, int(overslagen))
+                kleurenwiezen.update_score(spelers[1], spel, False, geslaagd, int(overslagen))
+                kleurenwiezen.update_score(spelers[2], spel, False, geslaagd, int(overslagen))
+            else:
+                kleurenwiezen.update_score(vrager1, spel, True, geslaagd)
+                spelers.remove(vrager1)
+                kleurenwiezen.update_score(spelers[0], spel, False, geslaagd)
+                kleurenwiezen.update_score(spelers[1], spel, False, geslaagd)
+                kleurenwiezen.update_score(spelers[2], spel, False, geslaagd)
+
+        kleurenwiezen.scores['Spel'] = spel
+        rondes.append(kleurenwiezen.score())
+
+    except Exception as e:
+        messagebox.showerror("Verkeerde input", str(e))
+
 
 
 
 nieuw_spel()
+print(kleurenwiezen.score())
+print(rondes)
